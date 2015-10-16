448c67da0af2499b2c85cead4fa9c415d9becee9
Matt Fellows <matt.fellows@onegeek.com.au>
2015-10-16 16:29:39 +1100

    Upgrade to Scala 2.11.x branch

--- src/main/g8/project/plugins.sbt
+++ src/main/g8/project/plugins.sbt
@@ -1,30 +1,24 @@
-addSbtPlugin("net.databinder.giter8" %% "giter8-plugin" % "0.6.4")
+addSbtPlugin("com.mojolly.scalate" % "xsbt-scalate-generator" % "0.4.2")
 
 addSbtPlugin("net.databinder.giter8" %% "giter8-scaffold" % "0.6.4")
 
-addSbtPlugin("com.mojolly.scalate" % "xsbt-scalate-generator" % "0.4.2")
-
 addSbtPlugin("org.scalatra.sbt" % "scalatra-sbt" % "0.3.2")
 
-addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.11.2")
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.0")
 
 resolvers += Resolver.url(
   "bintray-sbt-plugin-releases",
   url("http://dl.bintray.com/content/sbt/sbt-plugin-releases"))(
     Resolver.ivyStylePatterns)
 
-addSbtPlugin("me.lessis" % "bintray-sbt" % "0.1.1")
-
 resolvers += Classpaths.sbtPluginReleases
 
 resolvers += Classpaths.typesafeReleases
-
-addSbtPlugin("org.scoverage" %% "sbt-scoverage" % "0.99.5.1")
-
-addSbtPlugin("com.sksamuel.scoverage" %% "sbt-coveralls" % "0.0.5")
 
 addSbtPlugin("au.com.onegeek" % "sbt-dotenv" % "1.0.21")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "0.7.4")
 
 addSbtPlugin("com.heroku" % "sbt-heroku" % "0.4.1")
+
+addSbtPlugin("io.spray" % "sbt-revolver" % "0.7.2")
--- src/main/scaffolds/repository/src/main/scala/$package$/$class_name$.scala
+++ src/main/scaffolds/repository/src/main/scala/$package$/$class_name$.scala
@@ -8,15 +8,17 @@ import au.com.onegeek.respite.models.ModelJsonExtensions._
 import au.com.onegeek.respite.models.Model
 import play.api.libs.json.Json
 import reactivemongo.bson.BSONObjectID
-import uk.gov.hmrc.mongo.{ReactiveMongoFormats, ReactiveRepository, MongoConnector}
+import uk.gov.hmrc.mongo.{ReactiveMongoFormats, ReactiveRepository, MongoConnector, AtomicUpdate}
 import uk.gov.hmrc.mongo.ReactiveMongoFormats.objectIdFormats
 import au.com.onegeek.respite.models.ModelJsonExtensions._
 import au.com.onegeek.respite.controllers.support.{CachingRouteSupport, MetricsSupport, MetricsRestSupport}
 import $organisation$.model.$model_name$
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
  * Repository object for $model_name$
  */
 class $class_name$(implicit mc: MongoConnector)
-  extends ReactiveRepository[$model_name$, BSONObjectID]("$model_name;format="snake"$", mc.db, modelFormatForMongo {Json.format[$model_name$]}, ReactiveMongoFormats.objectIdFormats) {
+  extends ReactiveRepository[$model_name$, BSONObjectID]("$model_name;format="snake"$", mc.db, modelFormatForMongo {Json.format[$model_name$]}, ReactiveMongoFormats.objectIdFormats)
+    with AtomicUpdate[$model_name$] {
 }
